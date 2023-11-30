464087fce4fb06ff99a20356d850a2e34d3dec3d
Rdnbt <haroldbudd12@gmail.com>
2023-11-30 19:02:52 +0900

    Login Page Implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import LoginForm from './components/login/LoginForm';
+import RegisterForm from './components/login/RegisterForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="auth-form-container">
+        <LoginForm />
+        <RegisterForm />
+      </div>
     </div>
   );
 }
